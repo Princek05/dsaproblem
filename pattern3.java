b526840a48adc95557c97da7a8c43276208d32ef
prince <145844392+Princek05@users.noreply.github.com>
2024-11-27 16:32:44 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,14 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        // int row=5;
+        // int col=5;
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
